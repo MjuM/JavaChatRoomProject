9d8a370b36aa2c88a9536ae7282ffb4060eb996c
MjuM <maju@live.fr>
2020-06-19 23:28:52 +0200

    Initialisation/creation des methodes pour le serveur

---
+++ serveur/Server.java
@@ -0,0 +1,20 @@
+package serveur;
+
+public class Server {
+	public static void run(int port) {
+		
+	}
+	public static void arret() {
+		
+	}
+	private static void send() {
+		
+	}
+	private static void listen() {
+		
+	}
+	private static void broadcast() {
+		
+	}
+
+}
---
+++ serveur/ServerRoom.java
@@ -0,0 +1,8 @@
+package serveur;
+
+public class ServerRoom {
+public static void main(String[] args) {
+		
+	}
+
+}
