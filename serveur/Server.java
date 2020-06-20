205d1c48cea21911ac25fc95e4f657c0539bee51
MjuM <maju@live.fr>
2020-06-20 13:34:51 +0200

    Creation du thread pour l'écoute du serveur dans la classe serveur

--- serveur/Server.java
+++ serveur/Server.java
@@ -1,17 +1,21 @@
 package serveur;
 
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class Server {
 
+	private static boolean running;
 	
 	private static DatagramSocket mysocket;
 	
-	public static void run(int port) {
+	public static void launch(int port) {
 		try {
 			mysocket = new DatagramSocket(port);
-			System.out.println("Le serveur a été lancé sur le port: "+port);
 			
+			running = true;
+			listen();
+			System.out.println("Le serveur a été lancé sur le port: "+port);
 		
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -25,9 +29,28 @@ public class Server {
 		
 	}
 	private static void listen() {
-		
+		Thread listenThread = new Thread("JavaChatRoom Listener") {
+			public void run() {
+				try {
+					while(running) {
+						byte[] data = new byte[1024];
+						DatagramPacket packet = new DatagramPacket(data, data.length);
+						mysocket.receive(packet);
+						
+						String message = new String(data);
+						message = message.substring(0, message.indexOf("\\e")); // backslash e is the end of the msg sent by the data
+						
+						broadcast(message);
+					}
+					
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
+		}; listenThread.start();
 	}
-	private static void broadcast() {
+	private static void broadcast(String message) {
 		
 	}
 
--- serveur/ServerRoom.java
+++ serveur/ServerRoom.java
@@ -2,7 +2,7 @@ package serveur;
 
 public class ServerRoom {
 public static void main(String[] args) {
-		Server.run(400);
+		Server.launch(400);
 	}
 
 }
